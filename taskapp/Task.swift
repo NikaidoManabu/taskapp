5266507cd8ad57ceffe2f28017cd2903feb980ff
manabu.nikaido <nikaiodu.m.aa@m.titech.ac.jp>
2017-02-11 20:51:54 +0900

    commit

---
+++ taskapp/Task.swift
@@ -0,0 +1,33 @@
+//
+//  Task.swift
+//  taskapp
+//
+//  Created by nikaidomanabu on 2017/02/11.
+//  Copyright © 2017年 manabu.nikaido. All rights reserved.
+//
+
+import RealmSwift
+
+class Task: Object {
+    // 管理用 ID。プライマリーキー
+    dynamic var id = 0
+    
+    // タイトル
+    dynamic var title = ""
+    
+    // 内容
+    dynamic var contents = ""
+    
+    /// 日時
+    dynamic var date = NSDate()
+    
+    // 分類
+    dynamic var category = ""
+    
+    /**
+     id をプライマリーキーとして設定
+     */
+    override static func primaryKey() -> String? {
+        return "id"
+    }
+}
